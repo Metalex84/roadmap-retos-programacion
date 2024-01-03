527868b36af23e55e4d66c090a1e155fc07cc799
Alejandro Gonzalez Venegas <alex@MacBook-Air-de-Alejandro.local>
2024-01-03 16:37:47 +0100

    V1 - primeros menus

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/c/metalex84.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+void showMenuArithmetic()
+{
+    printf("***************************\n");
+    printf("Operadores aritmeticos en C\n");
+    printf("***************************\n");
+    printf("1. Suma\n");
+    printf("2. Resta\n");
+    printf("3. Multiplicacion\n");
+    printf("4. Division\n");
+    printf("5. Modulo\n");
+}
+
+void showMenuLogic()
+{
+    printf("***********************\n");
+    printf("Operadores logicos en C\n");
+    printf("***********************\n");
+    printf("1. OR\n");
+    printf("2. AND\n");
+    printf("3. NOT\n");
+    printf("4. NOR\n");
+    printf("5. NAND\n");
+}
+
+void showMenuComp()
+{
+    printf("****************************\n");
+    printf("Operadores comparativos en C\n");
+    printf("****************************\n");
+    printf("1. Mayor que\n");
+    printf("2. Menor que\n");
+    printf("3. Mayor o igual a\n");
+    printf("4. Menor o igual a\n");
+    printf("5. Igual\n");
+    printf("6. Distinto\n");
+}
+
+int main(void)
+{
+    return (0);
+}
